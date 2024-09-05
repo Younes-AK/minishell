f73f269df6ca6ec3a787906ad002940e135ceb29
Younes Akazdaou <yakazdao@e3r4p15.1337.ma>
2024-09-05 19:00:23 +0100

    kk

--- execution/exec_cmds.c
+++ execution/exec_cmds.c
@@ -37,28 +37,28 @@ char	*check_path(char **paths, char *cmd)
 	return (NULL);
 }
 
-static int check_command_status(char *cmd, t_prog *p)
+static int	check_command_status(char *cmd, t_prog *p)
 {
-    struct stat st;
+	struct stat	st;
 
-    if (!ft_strcmp(cmd, ""))
-        return (3);
-    if (is_all_slashes(cmd) || !ft_strcmp(cmd, ".."))
-        return (1);
-    p->access_path = check_path(p->all_paths, cmd);
-    if (stat(cmd, &st) == 0)
-    {
-        if (S_ISDIR(st.st_mode) && !(access(cmd, X_OK) == 0))
-            return (1);
-        if (access(cmd, X_OK) == 0)
-            return (5);
-        return (2);
-    }
-    if (p->access_path)
-        return (0);
-    if (errno == ENOENT)
-        return (3);
-    return (4);
+	if (!ft_strcmp(cmd, ""))
+		return (3);
+	if (is_all_slashes(cmd) || !ft_strcmp(cmd, ".."))
+		return (1);
+	p->access_path = check_path(p->all_paths, cmd);
+	if (stat(cmd, &st) == 0)
+	{
+		if (S_ISDIR(st.st_mode) && !(access(cmd, X_OK) == 0))
+			return (1);
+		if (access(cmd, X_OK) == 0)
+			return (5);
+		return (2);
+	}
+	if (p->access_path)
+		return (0);
+	if (errno == ENOENT)
+		return (3);
+	return (4);
 }
 
 static void	handle_command_errors(char **cmd, int cmd_status)
@@ -75,15 +75,9 @@ static void	handle_command_errors(char **cmd, int cmd_status)
 			error_msg1("command not found", cmd[0], 127);
 	}
 	else if (cmd_status == 4)
-	{
 		error_msg1(strerror(errno), cmd[0], 126);
-	}
 	else
 		return ;
-	if (cmd_status == 3)
-		exit(127);
-	else
-		exit(126);
 }
 
 static void	execute_cmd(char **cmd, t_prog *p)
@@ -100,6 +94,8 @@ static void	execute_cmd(char **cmd, t_prog *p)
 	if (p->access_path)
 	{
 		execve(p->access_path, cmd, p->env_variables);
+		if (p->cmd_status == 5)
+			error_msg1("is a directory", cmd[0], 126);
 		error_msg1(strerror(errno), cmd[0], 127);
 	}
 	error_msg2("command not found12", cmd[0]);
